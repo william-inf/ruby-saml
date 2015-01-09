d82f9863bb055b2d43b8295e1d351b39765976e4
WilliamThomas <williamt@infront.net.au>
2015-01-09 15:02:55 +1100

    fucked it

--- lib/xml_security.rb
+++ lib/xml_security.rb
@@ -110,9 +110,8 @@ module XMLSecurity
       # Add Transforms
       transforms_element = reference_element.add_element("ds:Transforms")
       transforms_element.add_element("ds:Transform", {"Algorithm" => ENVELOPED_SIG})
-      transforms_element.add_element("ds:Transform", {"Algorithm" => C14N})
-      transforms_element.add_element("ec:InclusiveNamespaces", {"xmlns" => C14N, "PrefixList" => INC_PREFIX_LIST})
-
+      transforms_element.add_element("ds:Transform", {"Algorithm" => C14N}, "ec:InclusiveNamespaces", {"xmlns" => C14N, "PrefixList" => INC_PREFIX_LIST})
+      # transforms_element.add_element("ec:InclusiveNamespaces", {"xmlns" => C14N, "PrefixList" => INC_PREFIX_LIST})
       digest_method_element = reference_element.add_element("ds:DigestMethod", {"Algorithm" => digest_method})
       reference_element.add_element("ds:DigestValue").text = compute_digest(canon_doc, algorithm(digest_method_element))
 
